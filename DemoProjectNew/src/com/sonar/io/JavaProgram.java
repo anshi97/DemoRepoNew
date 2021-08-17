16245e75888fd0d2cb67ef12de6b50bfb90380fc
anshu_sinha <asinha@kovair.com>
2021-08-17 06:19:17 -0700

    test

---
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -0,0 +1,42 @@
+package com.sonar.io;
+
+public class JavaProgram {
+	public static void main(String args[]){
+		int a[]={1,2,3,2,11,15,14,16};
+		System.out.println("Odd Numbers:");
+		for(int i=0;i<a.length;i++){
+			if(a[i]%2!=0){
+				System.out.println(a[i]);
+			}
+		}
+		System.out.println("Even Number:");
+	
+		for(int i=0;i<a.length;i++){
+			if(a[i]%2==0){
+				System.out.println(a[i]);
+			}
+		}
+		
+		
+		String input = "GeeksforGeeks"; 
+		  
+        // getBytes() method to convert string  
+        // into bytes[]...kk 
+        byte [] strAsByteArray = input.getBytes(); 
+  
+        byte [] result =  
+                   new byte [strAsByteArray.length]; 
+  
+        // Store result in reverse order into the 
+        // result sff byte[] 
+        for (int i = 0; i<strAsByteArray.length; i++) 
+            result[i] =  
+             strAsByteArray[strAsByteArray.length-i-1]; 
+  
+        System.out.println(new String(result)); 
+        System.out.println(new String(result)); 
+        // Requirement ID : 38
+		//mnlcd
+	}
+	
+}
