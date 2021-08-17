9c5e0c7e7e0f3fa597a7691c359ac3542e12f757
anshu_sinha <asinha@kovair.com>
2021-08-17 06:25:09 -0700

    kk

--- DemoProjectNew/src/com/sonar/io/JavaProgram.java
+++ DemoProjectNew/src/com/sonar/io/JavaProgram.java
@@ -21,7 +21,7 @@ public class JavaProgram {
 		String input = "GeeksforGeeks"; 
 		  
         // getBytes() method to convert string  
-        // into bytes[]...test code m
+        // into bytes[]...test code 
         byte [] strAsByteArray = input.getBytes(); 
   
         byte [] result =  
